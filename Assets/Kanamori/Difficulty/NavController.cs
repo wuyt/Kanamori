8965bd117ede09e3576dccc01680f9937d1f92e0
生活在他方 <5140075@qq.com>
2020-05-07 18:07:40 +0800

    导航难点接近示例
    利用NavMeshComponents导航。

---
+++ Assets/Kanamori/Difficulty/NavController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Kanamori.Difficulty
+{
+    public class NavController : MonoBehaviour
+    {
+        public Transform target;
+        public NavMeshAgent agent;
+        public LineRenderer lineRenderer;
+        private NavMeshPath path;
+        public Transform player;
+
+        void Start()
+        {
+            path = new NavMeshPath();
+        }
+
+        void Update()
+        {
+            //agent.SetDestination(target.position);
+            agent.enabled=false;
+            agent.transform.position=player.position;
+            agent.enabled=true;
+            agent.CalculatePath(target.position,path);
+            lineRenderer.positionCount = path.corners.Length;
+            lineRenderer.SetPositions(path.corners);
+        }
+    }
+}
+
