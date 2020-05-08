484b683b57d4e9b844f55c27638b372430ba83c8
生活在他方 <5140075@qq.com>
2020-05-08 09:15:43 +0800

    动态生成路径解决

---
+++ Assets/Kanamori/Difficulty/PathsController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Kanamori.Difficulty
+{
+    public class PathsController : MonoBehaviour
+    {
+        public Transform pa;
+        public Transform pb;
+        public Transform path;
+
+        void Update()
+        {
+            path.position = (pa.position + pb.position) / 2;
+            path.LookAt(pa);
+            path.localScale = new Vector3(0.03f, 1f, (pa.position - pb.position).magnitude * 0.1f);
+        }
+    }
+}
+
