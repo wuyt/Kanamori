0b3ad07a78edbaa47b157aa4f07848189df9cfdc
生活在他方 <5140075@qq.com>
2020-05-09 07:30:22 +0800

    方便调试场景的菜单场景完成

---
+++ Assets/Kanamori/Scripts/GameController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kanamori
+{
+    /// <summary>
+    /// 游戏控制器
+    /// </summary>
+    public class GameController : MonoBehaviour
+    {
+
+        private static GameController instance = null;
+
+        void Awake()
+        {
+            //实现单实例
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (this != instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+        /// <summary>
+        /// 获取稀疏空间地图名称
+        /// </summary>
+        /// <returns>地图名称</returns>
+        public string GetMapName()
+        {
+            return PlayerPrefs.GetString("MapName", "");
+        }
+        /// <summary>
+        /// 保存稀疏空间地图名称
+        /// </summary>
+        /// <param name="mapName">地图名称</param>
+        public void SaveMapName(string mapName)
+        {
+            PlayerPrefs.SetString("MapName", mapName);
+        }
+        /// <summary>
+        /// 获取稀疏空间地图ID
+        /// </summary>
+        /// <returns>地图ID</returns>
+        public string GetMapID()
+        {
+            return PlayerPrefs.GetString("MapID", "");
+        }
+        /// <summary>
+        /// 保存稀疏空间地图ID
+        /// </summary>
+        /// <param name="mapID">稀疏空间地图ID</param>
+        public void SaveMapID(string mapID)
+        {
+            PlayerPrefs.SetString("MapID", mapID);
+        }
+        /// <summary>
+        /// 删除地图
+        /// </summary>
+        public void DelMap(){
+            PlayerPrefs.DeleteKey("MapID");
+            PlayerPrefs.DeleteKey("MapName");
+        }
+
+    }
+}
+
---
+++ Assets/Kanamori/Scripts/MenuController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Kanamori
+{
+    public class MenuController : MonoBehaviour
+    {
+        /// <summary>
+        /// 添加地图按钮
+        /// </summary>
+        public Button btnAdd;
+        /// <summary>
+        /// 删除地图按钮
+        /// </summary>
+        public Button btnDel;
+        /// <summary>
+        /// 地图名称文本输入框
+        /// </summary>
+        public InputField input;
+        /// <summary>
+        /// 游戏控制
+        /// </summary>
+        private GameController game;
+        void Start()
+        {
+            game = FindObjectOfType<GameController>();
+            UISettings();
+        }
+        /// <summary>
+        /// 退出应用
+        /// </summary>
+        public void Quit()
+        {
+            Application.Quit();
+        }
+        /// <summary>
+        /// 加载场景
+        /// </summary>
+        /// <param name="sceneName">场景名称</param>
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        /// <summary>
+        /// 删除地图
+        /// </summary>
+        public void DeleteMap()
+        {
+            game.DelMap();
+            UISettings();
+        }
+        /// <summary>
+        /// 添加地图
+        /// </summary>
+        /// <param name="sceneName">场景名称</param>
+        public void AddMap(string sceneName)
+        {
+            if (input.text.Length > 0)
+            {
+                LoadScene(sceneName);
+            }
+        }
+        /// <summary>
+        /// UI显示控制
+        /// </summary>
+        private void UISettings()
+        {
+            if (game.GetMapID().Length == 0)
+            {
+                btnAdd.gameObject.SetActive(true);
+                btnDel.gameObject.SetActive(false);
+                input.text = "";
+                input.interactable = true;
+            }
+            else
+            {
+                btnAdd.gameObject.SetActive(false);
+                btnDel.gameObject.SetActive(true);
+                input.text = game.GetMapName();
+                input.interactable = false;
+            }
+        }
+    }
+}
+
